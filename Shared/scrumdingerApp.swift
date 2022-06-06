5c33a24513b02cd0bbe7e53e2d9e02f71d448e14
neezystudio <51094739+neezystudio@users.noreply.github.com>
2022-06-06 13:46:37 +0300

    Initial commit

---
+++ Shared/scrumdingerApp.swift
@@ -0,0 +1,20 @@
+//
+//  scrumdingerApp.swift
+//  Shared
+//
+//  Created by Victor Ndirangu on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
