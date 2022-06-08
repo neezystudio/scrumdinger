687869f3bfa24c1f4da4920bd43376a3d65ed744
neezystudio <51094739+neezystudio@users.noreply.github.com>
2022-06-08 10:03:51 +0300

    card view

---
+++ Shared/CardView.swift
@@ -0,0 +1,24 @@
+//
+//  CardView.swift
+//  scrumdinger
+//
+//  Created by Victor Ndirangu on 07/06/2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum:scrum)
+            .background(scrum.theme.mainColor)
+                        .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
